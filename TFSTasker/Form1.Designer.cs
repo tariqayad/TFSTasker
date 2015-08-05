45368639b27fecc057e999523c11828a61818462
Tariq <Tariqayad@gmail.com>
2015-08-05 10:52:40 +0530

    Initial Version

---
+++ TFSTasker/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace TFSTasker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbUSNo = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "User Story No";
+            // 
+            // tbUSNo
+            // 
+            this.tbUSNo.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbUSNo.Location = new System.Drawing.Point(91, 11);
+            this.tbUSNo.Name = "tbUSNo";
+            this.tbUSNo.Size = new System.Drawing.Size(158, 32);
+            this.tbUSNo.TabIndex = 1;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(15, 58);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(315, 199);
+            this.listBox1.TabIndex = 2;
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(255, 9);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(75, 41);
+            this.btnGo.TabIndex = 3;
+            this.btnGo.Text = "Create and Link";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(343, 261);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.tbUSNo);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbUSNo;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnGo;
+    }
+}
+
---
+++ TFSTasker/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Windows.Forms;
+using System.Xml;
+using Microsoft.TeamFoundation.Client;
+using Microsoft.TeamFoundation.WorkItemTracking.Client;
+
+namespace TFSTasker
+{
+    public partial class Form1 : Form
+    {
+        const string tfsUrlKey = "tfsProjectCollectionUrl";
+        const string projectNameKey = "projectName";
+
+        List<string> taskTitles;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            taskTitles = new List<string>();
+            GetTasks();
+
+        }
+
+        private void GetTasks()
+        {
+            XmlDocument tasksXml = new XmlDocument();
+            tasksXml.Load("tasks.xml");
+            var tasksNode = tasksXml.SelectSingleNode("Tasks");
+            foreach (XmlNode task in tasksNode.ChildNodes)
+            {
+                string taskTitle = task.Attributes["Title"].Value;
+                taskTitles.Add(taskTitle);
+            }
+        }
+
+        WorkItemStore workItemStore;
+        Project project;
+        Project GetProject()
+        {
+            if (this.project == null)
+            {
+                string tfsUrl = ConfigurationManager.AppSettings[tfsUrlKey];
+                string projectName = ConfigurationManager.AppSettings[projectNameKey];
+
+                var tfs = new TfsTeamProjectCollection(new Uri(tfsUrl));
+                tfs.EnsureAuthenticated();
+
+                workItemStore = tfs.GetService<WorkItemStore>();
+
+                this.project = workItemStore.Projects[projectName];
+            }
+
+            return project;
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            string usn = tbUSNo.Text;
+
+            if (!string.IsNullOrEmpty(usn))
+            {
+                AddLog("---------------------");
+                int workItemId = 0;
+
+                if (!Int32.TryParse(usn, out workItemId))
+                {
+                    return;
+                }
+
+                var project = GetProject();
+
+
+                // Get the user story
+                var backlogItem = GetBackLogItem(workItemId);
+                string iterationPath = backlogItem.IterationPath;
+                DialogResult doIt = MessageBox.Show("Link to " + backlogItem.Title, "Link to", MessageBoxButtons.YesNo);
+
+                if (doIt == System.Windows.Forms.DialogResult.Yes)
+                {
+                    foreach (string taskTitle in this.taskTitles)
+                    {
+                        var task = CreateTask(
+                            project,
+                            string.Format(taskTitle, backlogItem.Title),
+                            iterationPath);
+                        var taskLink = CreateTaskLink(task);
+                        UpdateBackLogItemLink(backlogItem, taskLink);
+                    }
+                }
+            }
+        }
+
+        private WorkItem GetBackLogItem(int workItemId)
+        {
+            var backlogItem = this.workItemStore.GetWorkItem(workItemId);
+            AddLog("Getting backlog item -" + backlogItem.Id + " - " + backlogItem.Title);
+            return backlogItem;
+        }
+
+        private void UpdateBackLogItemLink(WorkItem backlogItem, WorkItemLink taskLink1)
+        {
+            backlogItem.Links.Add(taskLink1);
+            AddLog("updating backlogworkitem link - " + taskLink1.TargetId.ToString() + "- " + taskLink1.SourceId.ToString());
+            backlogItem.Save();
+        }
+
+        private WorkItemLink CreateTaskLink(WorkItem task1)
+        {
+            var linkType = workItemStore.WorkItemLinkTypes["System.LinkTypes.Hierarchy"];
+            var taskLink = new WorkItemLink(linkType.ForwardEnd, task1.Id);
+
+            AddLog("created link- " + task1.Id.ToString());
+            return taskLink;
+        }
+
+        private WorkItem CreateTask(Project project, string taskTitle, string iterationPath)
+        {
+            // Create the tasks
+            var taskType = project.WorkItemTypes["Task"];
+            var task = new WorkItem(taskType);
+            task.IterationPath = iterationPath;
+            //task.State = "New";
+
+            task.Title = taskTitle;
+            task.Save();
+
+            AddLog("created task - " + task.Id.ToString());
+            return task;
+        }
+
+        private void AddLog(string text)
+        {
+            listBox1.Items.Add(text);
+            Console.WriteLine(text);
+        }
+    }
+}
